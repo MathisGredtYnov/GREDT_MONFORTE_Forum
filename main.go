1e6e7aa97c0919db46a491e88ccc1f2f5f54246d
MathisGredtYnov <113118860+MathisGredtYnov@users.noreply.github.com>
2023-06-01 15:32:06 +0200

    server go + nomenclature

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("server is running on port 8080 http://localhost:8080")
+	//gestion des css
+	http.Handle("/CSS/", http.StripPrefix("/CSS/", http.FileServer(http.Dir("CSS"))))
+	//gestion des assets
+	http.Handle("/ASSETS/", http.StripPrefix("/ASSETS/", http.FileServer(http.Dir("ASSETS"))))
+	//gestion des images
+	http.Handle("/ASSETS/image", http.StripPrefix("/ASSETS/IMG", http.FileServer(http.Dir("img"))))
+	//gestion des pages
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/Private", Private)
+	http.HandleFunc("/Compte", Compte)
+	http.HandleFunc("/CGU", CGU)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+	tmp := template.Must(template.ParseFiles("FRONTEND/template/index.html"))
+	tmp.Execute(w, nil)
+}
+
+func Private(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/Private" {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+	tmp := template.Must(template.ParseFiles("FRONTEND/template/Private.html"))
+	tmp.Execute(w, nil)
+}
+
+func Compte(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/Compte" {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+	tmp := template.Must(template.ParseFiles("FRONTEND/template/Compte.html"))
+	tmp.Execute(w, nil)
+}
+
+func CGU(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/CGU" {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+	tmp := template.Must(template.ParseFiles("FRONTEND/template/CGU.html"))
+	tmp.Execute(w, nil)
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprint(w, "404 not found")
+	}
+}
