1981fcc4153b3d257ab0d9afcc7ddcc1a61c2ef2
MONFORTE Pierre <95189614+BUSTERDAY@users.noreply.github.com>
2023-06-05 07:57:20 +0200

    appel base de données et docs

---
+++ AppelBaseDeDonnées.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/projet_forum")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT ID_user, pseudonyme FROM utilisateur WHERE ID_user = 1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		if err := rows.Scan(&id, &name); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, name)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module programmeliaisonMySQLGO
+
+go 1.19
+
+require github.com/go-sql-driver/mysql v1.7.1
+
+
