f7d6299ebe7bc5747521911e2d9544c9a060db6e
MathisGredtYnov <113118860+MathisGredtYnov@users.noreply.github.com>
2023-06-13 15:52:23 +0200

    ajout donnée sur le topic selectionné

--- FRONTEND/JS/script.js
+++ FRONTEND/JS/script.js
@@ -7,26 +7,28 @@ function toggleDropdown(element) {
 function showTopic(topicIndex) {
     var divTopicElement = document.querySelector(".div-topic");
     var topicDetailsElement = document.getElementById("topic-details");
-    var topicContentElement = document.querySelector("#topic-" + topicIndex + " .topic-content");
-    var topicTitleElement = document.querySelector("#topic-" + topicIndex + " .topic-title");
-    var topicPseudoElement = document.querySelector("#topic-" + topicIndex + " .topic-pseudo-first-message");
+    var topicContentElement = topicDetailsElement.querySelector(".topic-content span");
+    var topicTitleElement = topicDetailsElement.querySelector(".topic-title span");
+    var topicPseudoElement = topicDetailsElement.querySelector(".topic-pseudo-first-message span");
 
     divTopicElement.style.display = "none";
     topicDetailsElement.style.display = "flex";
 
-    var topicContent = topicContentElement.innerText;
-    var topicTitle = topicTitleElement.innerText;
-    var topicPseudo = topicPseudoElement.innerText;
+    var topicElements = document.getElementsByClassName("topic");
+    var topic = topicElements[topicIndex];
+
+    var topicContent = topic.querySelector(".topic-content span").innerHTML;
+    var topicTitle = topic.querySelector(".topic-title span").innerHTML;
+    var topicPseudo = ""; // Assurez-vous de définir correctement la valeur de topicPseudo
 
     var topicContentElementDetails = topicDetailsElement.querySelector(".topic-content");
     var topicFirstMessageElement = topicDetailsElement.querySelector(".topic-first-message");
     var topicPseudoFirstMessageElement = topicDetailsElement.querySelector(".topic-pseudo-first-message");
 
-    topicContentElementDetails.innerText = topicContent;
-    topicFirstMessageElement.innerText = topicTitle;
-    topicPseudoFirstMessageElement.innerText = topicPseudo;
+    topicContentElementDetails.innerHTML = "<p id=\"red_arrow\">&#10173;</p><span>" + topicContent + "</span>";
+    topicFirstMessageElement.innerHTML = "<p id=\"blue_arrow\">&#10173;</p><span>" + topicTitle + "</span>";
+    topicPseudoFirstMessageElement.innerHTML = "<span>" + topicPseudo + "</span>";
 }
-
 
 
 
